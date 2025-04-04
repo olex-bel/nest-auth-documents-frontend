44a0b2552aed1a39a1530ffc76fe4155f391e65a
olex-bel <ban_konung@ukr.net>
2025-04-04 21:19:53 +0200

    Changd base path.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/document-editor",
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
